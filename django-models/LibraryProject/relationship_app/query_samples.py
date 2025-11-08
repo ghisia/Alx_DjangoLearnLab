2d355aa6f7acccf60406806d5229e5a2a7b4aeb9
ghisia <61737335+ghisia@users.noreply.github.com>
2025-11-08 18:13:35 +0200

    Refactor query_samples.py to use variables for names

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,12 +8,14 @@ django.setup()
 from relationship_app.models import Author, Book, Library, Librarian
 
 # 1. Query all books by a specific author
-author = Author.objects.get(name="J.K. Rowling")
+author_name = "J.K. Rowling"  # Use a variable
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
 print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
 
 # 2. List all books in a library
-library = Library.objects.get(name="Central Library")
+library_name = "Central Library"  # Use a variable
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all()
 print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
 
