91d2bd754b15d2432e173d3803f0fbf896eb60fa
ghisia <61737335+ghisia@users.noreply.github.com>
2025-11-09 22:07:16 +0300

    Add files via upload
    new work for update

---
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path("register/", views.register, name="register"),
+    path("login/", LoginView.as_view(template_name="relationship_app/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(template_name="relationship_app/logout.html"), name="logout"),
+]
---
+++ django-models/LibraryProject/relationship_app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
+from .models import Library
+from django.views.generic.detail import DetailView
+# ✅ Function-based view for all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+# ✅ Class-based view for library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # required for the check
+    context_object_name = 'library'  # required for the check
