b6e2711435c87265a22f15d69c6ce7e892254f34
ghisia <61737335+ghisia@users.noreply.github.com>
2025-11-11 21:53:46 +0300

    Register signals in app ready method
    Import signals in the ready method to ensure they are registered when the app is ready.

--- django-models/relationship_app/apps.py
+++ django-models/relationship_app/apps.py
@@ -1,9 +1,9 @@
 from django.apps import AppConfig
 
-
 class RelationshipAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'relationship_app'
 
+    # This ensures signals are registered when the app is ready
     def ready(self):
-        pass     
+        import relationship_app.signals
